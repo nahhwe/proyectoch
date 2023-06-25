ecec9a3b402ed0a894e99c7436c384295a30cc20
Nahuel Soria <59202972+nahhwe@users.noreply.github.com>
2023-06-24 21:06:12 -0300

    Proyecto Final modificado.
    Proyecto Final con las correcciones realizadas.

--- app.js
+++ app.js
@@ -1,80 +1,175 @@
-const carrito = [];
+let carrito = [];
+let subtotal = 0;
 
 let rangos = [
-    {id:1 , titulo:"Ascendant 3" , precio:5000},
-    {id:2 , titulo: "Inmortal 3" , precio:10000},
-    {id:3 , titulo:"Radiant" , precio: 15000},
+    {id:0, titulo: "Ascendant 3", precio:5000, img: "./img/Ascendant.png"} ,
+    {id:1, titulo: "Inmortal 3", precio:10000, img: "./img/Inmortal.png"},
+    {id:2, titulo:"Radiant", precio: 15000, img: "./img/Radiant.jpg"},
 ]
+
+const contenedorRangos = document.querySelector("#contenedor-producto")
+
+function cargarProductos(){
+    rangos.forEach(item => {
+        const div = document.createElement("div")
+        div.classList.add("container-cards")
+        div.innerHTML = `
+        <div class="card border-dark mb-3" style="max-width: 20rem">
+        <img src="${item.img}" style="max-width: 20rem">
+            <div class="card-header">${item.titulo}</div>
+            <div class="card-body">
+                <p class="card-text">Llevamos tu cuenta al rango competitivo ${item.titulo}.</p>
+                <p class="card-text">$${item.precio}.</p>
+                    <button type="button" class="producto-agregar btn btn-dark" id="${item.id}">Agregar</button>
+                </div>
+            </div>
+        </div>
+        `;
+        contenedorRangos.append(div)
+    })   
+}
+
+cargarProductos();
+
+let botonesAgregar = document.querySelectorAll(".producto-agregar")
+
+botonesAgregar.forEach(boton =>{
+    boton.addEventListener("click", agregarAlCarrito);
+})
+
+function agregarAlCarrito(e){
+    const id = e.currentTarget.id
+    const rangoAgregado = rangos.find(rango => rango.id == id)
+    
+    if(carrito.some(item => item.id == id)){
+        const index = carrito.findIndex(rango => rango.id == id)
+        carrito[index].cantidad++
+    }else{
+        rangoAgregado.cantidad = 1;
+        carrito.push(rangoAgregado);
+    }
+    mostrarCarrito();
+    Toastify({
+        text: "Item agregado al carrito.",
+        duration: 5000,
+        newWindow: true,
+        close: true,
+        gravity: "bottom",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function(){}
+      }).showToast();
+}
 
 let topTen = {id:4 , titulo:"TOP TEN" , precio:30000}
 
-// Capturar el boton de Agregar al carrito.
+ // MOSTRAR CARRITO.
 
-const agregarProductoAscendant = document.querySelector("#agregaritemascendant"); 
-console.log(agregarProductoAscendant);
+const mostrarCart = document.getElementById('carrito')
+const mostrarSub = document.getElementById('subtotal')
 
-// Agregar evento al boton para que agregue un producto al arreglo (carrito)
+function mostrarCarrito(arr){
+    mostrarCart.innerHTML = ""
+    if(carrito.length === 0){
+        mostrarCart.innerHTML = `El carrito esta vacío.`        
+    }else{
+        carrito.forEach(producto => {
+            subtotal = carrito.reduce((acc, item) => {
+                return acc += (item.precio * item.cantidad)
+            }, 0);
+            const div = document.createElement("div")
+            div.classList.add("container-cart")
+            div.innerHTML = `
+            <p>${producto.titulo}</p>
+            <p>${producto.cantidad}</p>
+            <p>$${producto.precio * producto.cantidad}</p>
+            `
+                    mostrarCart.append(div)
 
-agregarProductoAscendant.addEventListener("click", ()=>{
-     console.log("Agregaste Ascendant 3 al carrito!");
-     carrito.push(rangos[0]);
-     console.log(carrito);
- })
+        });
+    }
+    mostrarSubtotal();
+}
 
- // Capturar el boton de Agregar al carrito.
+mostrarCarrito(carrito)
 
-const agregarProductoInmortal = document.querySelector("#agregariteminmortal"); 
-console.log(agregarProductoInmortal);
+//MOSTRAR SUBTOTAL
 
-// Agregar evento al boton para que agregue un producto al arreglo (carrito)
+function mostrarSubtotal(suma){        
+    if(suma === 0){
+        mostrarSub.innerHTML = ""
+    }else{
+        mostrarSub.innerHTML = `Total: $${subtotal}`
+    }
+}
 
-agregarProductoInmortal.addEventListener("click", ()=>{
-     console.log("Agregaste Inmortal 3 al carrito!");
-     carrito.push(rangos[1]);
-     console.log(carrito);
- })
+mostrarSubtotal(subtotal)
 
-  // Capturar el boton de Agregar al carrito.
+// VACIAR CARRITO
 
-const agregarProductoRadiant = document.querySelector("#agregaritemradiant"); 
-console.log(agregarProductoRadiant);
-
-// Agregar evento al boton para que agregue un producto al arreglo (carrito)
-
-agregarProductoRadiant.addEventListener("click", ()=>{
-     console.log("Agregaste Radiant al carrito!");
-     carrito.push(rangos[2]);
-     console.log(carrito);     
- })
+let carritoVacio = document.querySelector("#vaciar-carrito")
+carritoVacio.addEventListener("click",()=>{
+    event.preventDefault()
+    if(carrito.length !== 0){
+    carrito = [];
+    mostrarCarrito();
+    mostrarSub.innerHTML = ""
+    Toastify({
+        text: "Has eliminado todos los items del carrito!",
+        duration: 10000,
+        newWindow: true,
+        close: true,
+        gravity: "bottom",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+            background: "peru",
+        },
+        onClick: function(){}
+      }).showToast();
+      }
+})
 
  // Capturar el boton de Compra.
 
  const comprar = document.querySelector("#botoncomprar")
- console.log(comprar)
 
- // Agregar evento al boton para que guarde la información de contacto. En este caso se guarda dentro de una variable.
+ // Agregar evento al boton para que guarde la información de contacto. En este caso se guarda en el localStorage.
 
  comprar.addEventListener("click", () =>{
     event.preventDefault()
    let nombrecliente = document.getElementById('inputName').value
    let emailcliente = document.getElementById('inputEmail').value
-   console.log(nombrecliente)
-   console.log(emailcliente)
+       Toastify({
+        text: "Compra realizada! Pronto recibiar un e-mail con la factura.",
+        duration: 10000,
+        newWindow: true,
+        close: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: "center", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: "green",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+    localStorage.setItem("carrito-comprado", JSON.stringify(carrito));
+    localStorage.setItem("nombre-cliente-carritoOK", JSON.stringify(nombrecliente));
+    localStorage.setItem("email-cliente-carritoOK", JSON.stringify(emailcliente));
  })
 
 const rangosJSON = JSON.stringify(rangos); // Paso del arreglo "rangos" a un JSON - Texto plano.
-console.log(rangosJSON); // Muestro un JSON - Texto plano.
 
 localStorage.setItem("rangos", rangosJSON); // Envio key.value al localStorage.
 
 const rangosActualizados = JSON.parse(localStorage.getItem("rangos")); // Traigo "rangos" del localStorage en formato de arreglo.
-console.log(rangosActualizados); //Muestro el arreglo que traje del localStorage.
 
 rangosActualizados.push(topTen); //Envio un nuevo producto/servicio al arreglo.
-console.log(rangosActualizados); //Muestro el arreglo modificado.
 
 const rangosActualizadosJSON = JSON.stringify(rangosActualizados); // Paso del arreglo modificado a un JSON - Texto plano.
-console.log(rangosActualizadosJSON); //Muestro el JSON - Texto plano.
 
 localStorage.setItem("rangos2", rangosActualizadosJSON); // Envio key.value (modificado) al localStorage.
 
@@ -100,11 +195,9 @@ fetch(url)
     .catch(error => console.log(error))
     
 const obtenerDatos = (data) =>{
-    //console.log(data.data[4].tiers)
     const { tiers } = data.data[4]
     let body = ''
     for(let i = 21; i < tiers.length; i++){
-        //console.log(tiers[i].tierName)
         body += `<img src= "${tiers[i].smallIcon}" witdh=50px height=50px> ${tiers[i].tierName} - `
         }
     document.getElementById('rangosAPI').innerHTML = body
